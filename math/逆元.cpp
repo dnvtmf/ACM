ea20812180dd7d8193319da9328b8a27f89f4551
dnvtmf <dnvtmf@gmail.com>
2015-06-21 09:35:20 +0800

    add some math about mod

--- math/逆元.cpp
+++ math/逆元.cpp
@@ -6,60 +6,62 @@
 //O(n)  预处理inv[1-n]：O（n^2)
 LL getInv(LL x, LL MOD)
 {
-	for(LL i = 1; i < MOD; i++)
-		if(x * i % MOD == 1)
-			return i;
-	return -1;
+    for(LL i = 1; i < MOD; i++)
+        if(x * i % MOD == 1)
+            return i;
+    return -1;
 }
 
 //方法二： 费马小定理和欧拉定理
 //费马小定理:@$ a^{(p-1)} \equiv 1(\% p) $@, 其中p是质数, 所以a的逆元是@$a^{(p - 2)}\%p$@
 //欧拉定理:@$ x^{\phi(m)} \equiv 1 (\% m)$@ x与m互素, m是任意整数
 //O(log n)(配合快速幂), 预处理inv[1-n]：O（nlog n)
-LL qpow(LL x, LL k, LL MOD){....}
+LL qpow(LL x, LL k, LL MOD) {....}
 LL getInv(LL x, LL MOD)
 {
-	//return qpow(x, euler_phi(MOD) - 1, MOD);
-	return qpow(x, MOD - 2, MOD);//MOD是质数
+    //return qpow(x, euler_phi(MOD) - 1, MOD);
+    return qpow(x, MOD - 2, MOD);//MOD是质数
 }
 
 //方法三：扩展欧几里得算法
 //扩展欧几里得算法可解决  @$ a*x + b * y = gcd(a, b) $@
 //所以a * x % MOD = gcd(a, b) % MOD (b = MOD)
 //O(log n), 预处理inv[1-n]：O(nlog n)
-LL exgcd(LL a, LL b, LL &x, LL &y){
-	if(b == 0)
-	{
-		x = 1;
-		y = 0;
-		return a;
-	}
-	LL g = exgcd(b, a%b, x, y);
-	LL t = x;
-	x = y;
-	y = t - a / b * y;
-	return g;
+inline void exgcd(LL a, LL b, LL &g, LL &x, LL &y)
+{
+    if(!b) g = x, x = 1, y = 0;
+    else exgcd(b, a % b, g, y, x), y -= (a / b) * x;
 }
-LL getInv(LL x, LL MOD){
-	LL inv, y;
-	exgcd(x, MOD, inv, y);
-	inv = (inv%MOD + MOD)%MOD;
-	return inv;
+
+LL getInv(LL x, LL mod)
+{
+    LL g, inv, tmp;
+    exgcd(x, mod, g, inv, tmp);
+    return g != 1 ? -1 : (inv + mod) % mod;
 }
 
 //方法四：积性函数
-//已处理inv[1] -- inv[n - 1], 求inv[n]， （MOD > n)
+//已处理inv[1] -- inv[n - 1], 求inv[n]， （MOD > n) (MOD为质数,不存在逆元的i干扰结果)
 //MOD = x*n - y(0 <= y < n), ==> x * n = y (% MOD), ==> x * n * inv[y] = y * inv[y] = 1 (%MOD)
 //所以inv[n] = x * inv[y] (x = MOD - MOD / n, y  MOD % n)
 //O(log n) 预处理inv[1-n]：O(n)
 LL inv[NUM];
 void inv_pre(LL mod)
 {
-	inv[0] = inv[1] = 1LL;
-	for(int i = 2; i < NUM; i++)
-		inv[i] = (mod - mod/i)*inv[mod%i] % mod;
+    inv[0] = inv[1] = 1LL;
+    for(int i = 2; i < NUM; i++)
+        inv[i] = (mod - mod / i) * inv[mod % i] % mod;
 }
-
+LL getInv(LL x, LL mod)
+{
+    LL res = 1LL;
+    while(x > 1)
+    {
+        res = res * (mod - mod / x) % mod;
+        x  = mod % x;
+    }
+    return res;
+}
 //方法五：积性函数+因式分解
 //预处理出所有质数的的逆元，采用exgcd来实现素数O(log n)求逆
 //采用质因数分解， 可在O(log n)求出任意一个数的逆元
